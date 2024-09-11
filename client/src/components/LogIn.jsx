bf4c7d213b5f6dfb276b0a43bddb046c5be09e66
Gabe Harvey <harvey7@gmail.com>
2024-09-10 19:03:17 -0500

    Updated styling on LogIn.jsx and SignUp.jsx

--- client/src/components/LogIn.jsx
+++ client/src/components/LogIn.jsx
@@ -1,16 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
-import { useState } from 'react';
 import { Box, Heading, Button, Input, FormControl, FormLabel, Image } from '@chakra-ui/react';
-import { useSpring, animated } from '@react-spring/web';
 
 const LogIn = () => {
-  const [flip, setFlip] = useState(false);
-
-  const flipProps = useSpring({
-    transform: `perspective(600px) rotateY(${flip ? 180 : 0}deg)`,
-    config: { mass: 5, tension: 800, friction: 80 },
-  });
-
   return (
     <Box
       maxW="1280px"
@@ -29,33 +20,30 @@ const LogIn = () => {
         Welcome Back
       </Heading>
 
-      <animated.div style={{ transformStyle: 'preserve-3d', ...flipProps }}>
-        <Box
-          bg="white"
-          p="2rem"
-          borderRadius="15px"
-          boxShadow="0px 4px 10px rgba(0, 0, 0, 0.1)"
-          maxWidth="500px"
-          mx="auto"
-          onClick={() => setFlip(!flip)}
-        >
-          <FormControl mb="1rem">
-            <FormLabel>Email</FormLabel>
-            <Input type="email" placeholder="Enter your email" />
-          </FormControl>
-          <FormControl mb="1rem">
-            <FormLabel>Password</FormLabel>
-            <Input type="password" placeholder="Enter your password" />
-          </FormControl>
-          <Button colorScheme="teal" width="full">
-            Log In
-          </Button>
-        </Box>
-      </animated.div>
+      <Box
+        bg="#CA85A0"
+        p="2rem"
+        borderRadius="15px"
+        boxShadow="0px 4px 10px rgba(0, 0, 0, 0.1)"
+        maxWidth="500px"
+        mx="auto"
+      >
+        <FormControl mb="1rem" fontFamily="'Shadows Into Light Two', cursive" fontWeight="bold" color="beige">
+          <FormLabel fontWeight="bold">Email</FormLabel>
+          <Input type="email" placeholder="Enter your email" backgroundColor="white" color="black" fontWeight="bold" />
+        </FormControl>
+        <FormControl mb="1rem" fontFamily="'Shadows Into Light Two', cursive" fontWeight="bold" color="beige">
+          <FormLabel fontWeight="bold">Password</FormLabel>
+          <Input type="password" placeholder="Enter your password" backgroundColor="white" color="black" fontWeight="bold" />
+        </FormControl>
+        <Button colorScheme="whiteAlpha" width="full" fontFamily="'Shadows Into Light Two', cursive" color="beige">
+          Log In
+        </Button>
+      </Box>
 
       <Box mt="3rem">
         <Image
-          src="/login-footer-image.png"
+          src="/login-footer-image.png" 
           alt="Log In Footer"
           width="100%"
           height="auto"
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,16 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
-import { useState } from 'react';
 import { Box, Heading, Button, Input, FormControl, FormLabel, Image } from '@chakra-ui/react';
-import { useSpring, animated } from '@react-spring/web';
 
 const SignUp = () => {
-  const [flip, setFlip] = useState(false);
-
-  const flipProps = useSpring({
-    transform: `perspective(600px) rotateY(${flip ? 180 : 0}deg)`,
-    config: { mass: 5, tension: 800, friction: 80 },
-  });
-
   return (
     <Box
       maxW="1280px"
@@ -19,43 +10,44 @@ const SignUp = () => {
       py="4rem"
       textAlign="center"
       bg="#F6CBD4"
-      backgroundImage="url('/signup-background.png')"
+      backgroundImage="url('/signup-background.png')" 
       backgroundPosition="center top"
       backgroundRepeat="no-repeat"
       backgroundSize="contain"
       mt={20}
     >
       <Heading as="h1" size="xl" mb="1rem" fontFamily="'Shadows Into Light Two', cursive" color="#A66A8A" fontWeight="900">
-        Create Your Account
+        Create an Account
       </Heading>
 
-      <animated.div style={{ transformStyle: 'preserve-3d', ...flipProps }}>
-        <Box
-          bg="white"
-          p="2rem"
-          borderRadius="15px"
-          boxShadow="0px 4px 10px rgba(0, 0, 0, 0.1)"
-          maxWidth="500px"
-          mx="auto"
-          onClick={() => setFlip(!flip)}
-        >
-          <FormControl mb="1rem">
-            <FormLabel>Email</FormLabel>
-            <Input type="email" placeholder="Enter your email" />
-          </FormControl>
-          <FormControl mb="1rem">
-            <FormLabel>Password</FormLabel>
-            <Input type="password" placeholder="Enter your password" />
-          </FormControl>
-          <Button colorScheme="teal" width="full">
-            Sign Up
-          </Button>
-        </Box>
-      </animated.div>
+      <Box
+        bg="#CA85A0"
+        p="2rem"
+        borderRadius="15px"
+        boxShadow="0px 4px 10px rgba(0, 0, 0, 0.1)"
+        maxWidth="500px"
+        mx="auto"
+      >
+        <FormControl mb="1rem" fontFamily="'Shadows Into Light Two', cursive" fontWeight="bold" color="beige">
+          <FormLabel fontWeight="bold">Name</FormLabel>
+          <Input type="text" placeholder="Enter your name" backgroundColor="white" color="black" fontWeight="bold" />
+        </FormControl>
+        <FormControl mb="1rem" fontFamily="'Shadows Into Light Two', cursive" fontWeight="bold" color="beige">
+          <FormLabel fontWeight="bold">Email</FormLabel>
+          <Input type="email" placeholder="Enter your email" backgroundColor="white" color="black" fontWeight="bold" />
+        </FormControl>
+        <FormControl mb="1rem" fontFamily="'Shadows Into Light Two', cursive" fontWeight="bold" color="beige">
+          <FormLabel fontWeight="bold">Password</FormLabel>
+          <Input type="password" placeholder="Enter your password" backgroundColor="white" color="black" fontWeight="bold" />
+        </FormControl>
+        <Button colorScheme="whiteAlpha" width="full" fontFamily="'Shadows Into Light Two', cursive" color="beige">
+          Sign Up
+        </Button>
+      </Box>
 
       <Box mt="3rem">
         <Image
-          src="/signup-footer-image.png"
+          src="/signup-footer-image.png" // Footer image specific to Sign Up if needed
           alt="Sign Up Footer"
           width="100%"
           height="auto"
